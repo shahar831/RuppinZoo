885bbc54a955a683fd242cf54dc2787802b20c64
Shahar831 <shahar.hekter@gmail.com>
2024-03-11 22:37:12 +0200

    fixes signup

--- signup.js
+++ signup.js
@@ -1,6 +1,31 @@
 const nameInput = document.getElementById("fname");
 
-function createNewVisitor(event) {
+function initializeFormEventListeners() {
+  const submitButton = document.getElementById("signup__submit");
+  const loginButton = document.getElementById("button__login");
+  let submitClicked = false;
+
+  if (submitButton) {
+    submitButton.addEventListener("click", () => {
+      submitClicked = true;
+    });
+  }
+
+  if (loginButton) {
+    loginButton.addEventListener("click", () => {
+      submitClicked = false;
+    });
+  }
+
+  const form = document.getElementById("create-visitor-form");
+  if (form) {
+    form.addEventListener("submit", (event) =>
+      createNewVisitor(event, submitClicked)
+    );
+  }
+}
+
+/**function createNewVisitor(event) {
   event.preventDefault();
 
   let validInput = validateFormInputs();
@@ -26,16 +51,54 @@ function createNewVisitor(event) {
   if (validInput) {
     makeVisitor(nameInput.value, selectedImg);
   }
+}**/
+
+function createNewVisitor(event, submitClicked) {
+  event.preventDefault();
+
+  if (!submitClicked) {
+    return;
+  }
+
+  if (!validateFormInputs()) {
+    return;
+  }
+
+  let selectedGenderElement = document.querySelector(
+    'input[name="gender"]:checked'
+  );
+  let selectedImg;
+
+  if (selectedGenderElement) {
+    let selectedGender = selectedGenderElement.value;
+    selectedImg =
+      selectedGender === "Male"
+        ? "./Animals_pictures/boy_4140068.png"
+        : selectedGender === "Female"
+        ? "./Animals_pictures/girl_4140076.png"
+        : "./Animals_pictures/user_1144709.png";
+  } else {
+    selectedImg = "./Animals_pictures/user_1144709.png"; // Default image if no gender selected
+  }
+
+  makeVisitor(nameInput.value, selectedImg);
 }
 
 const validateFormInputs = () => {
+  if (!nameInput.value) {
+    alert("You must provide your personal details");
+    return false;
+  }
+  return true;
+};
+/**const validateFormInputs = () => {
   if (!nameInput.value) {
     // validate there is text in the inputs
     alert("You must provide your personal details");
     return false;
   }
   return true;
-};
+};**/
 
 const makeVisitor = (name, selectedImg) => {
   let foundVisitor = visitors.find((visitor) => visitor.name === name);
@@ -66,3 +129,5 @@ const createForm = document.getElementById("create-visitor-form");
 if (createForm) {
   createForm.addEventListener("submit", createNewVisitor);
 }
+
+document.addEventListener("DOMContentLoaded", initializeFormEventListeners);
